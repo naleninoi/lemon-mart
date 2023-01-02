85e7b91e01cd1dab70462b687f9c9dd1bd65f57a
Viktor Kochetov-Arhipov <naleninoi@gmail.com>
2023-01-02 19:44:07 +0300

    Manager dashboard skeleton

--- src/app/manager/manager.module.ts
+++ src/app/manager/manager.module.ts
@@ -3,13 +3,18 @@ import { CommonModule } from '@angular/common';
 
 import { ManagerRoutingModule } from './manager-routing.module';
 import { ManagerHomeComponent } from './manager-home/manager-home.component';
+import { ManagerComponent } from './manager/manager.component';
+import { MaterialModule } from '../material.module';
+import { UserManagementComponent } from './user-management/user-management.component';
+import { ReceiptLookupComponent } from './receipt-lookup/receipt-lookup.component';
 
 
 @NgModule({
-  declarations: [ManagerHomeComponent],
+  declarations: [ManagerHomeComponent, ManagerComponent, UserManagementComponent, ReceiptLookupComponent],
   imports: [
     CommonModule,
-    ManagerRoutingModule
+    ManagerRoutingModule,
+    MaterialModule
   ]
 })
 export class ManagerModule { }
